2111140a7ca85abc6d0b8198f14e7c77612e811e
nachmatt <nmatonte@gmail.com>
2022-12-22 17:07:51 -0300

    footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div className='footer-wrapper'>
             <Link to='/' style={{ textDecoration: 'none' }}>
-                <h2 className='title'>PC Parts <span>PC Parts</span></h2>
+                <h2 className='footer-title'>PC Parts</h2>
             </Link>
         </div>
     )
